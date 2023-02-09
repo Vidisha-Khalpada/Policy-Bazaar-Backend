8203692e9aa2a51dbe272a988e99b16766b1e769
Vidisha-Khalpada <vidishakhalpada@gmail.com>
2023-02-09 19:28:28 +0530

    User and policies realtionship added

--- Server/Routes/Routes.js
+++ Server/Routes/Routes.js
@@ -1,5 +1,8 @@
 const express=require("express")
+const authenticate = require("../Middleware/authentication")
+const InsuranceModel = require("../Models/Insurance")
 const NavbarModel = require("../Models/Navbar")
+const UserInsuranceModel = require("../Models/UserInsurance")
 
 const router=express.Router()
 router.get("/navbar",async(req,res)=>
@@ -21,6 +24,41 @@ router.post("/navbar",async(req,res)=>
         let data=await NavbarModel.create(req.body)
         res.send({
             data:data
+        })
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+router.post("/insurance",async(req,res)=>
+{
+    try {
+        let data=await InsuranceModel.create(req.body)
+        res.send({
+            data:data
+        })
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
+router.get("/allinsurance",async(req,res)=>
+{
+    try {
+        let data=await InsuranceModel.find({})
+        res.send({
+            data:data
+        })
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
+router.post("/addinsurance",authenticate,async(req,res)=>
+{
+    try {
+        await UserInsuranceModel.create(req.body)
+        res.send({
+            "message":"The data is successfully added."
         })
     } catch (error) {
         res.status(400).send(error)
